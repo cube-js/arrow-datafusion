apache/datafusion

Add SQL planner support for calling `round` function with two arguments (#2503)
* test

* specify correct signature for ROUND function

datafusion/core/src/sql/planner.rs
@@ -4355,6 +4355,10 @@ mod tests {
                     Field::new("j3_id", DataType::Int32, false),
                     Field::new("j3_string", DataType::Utf8, false),
                 ])),
+                "test_decimal" => Ok(Schema::new(vec![
+                    Field::new("id", DataType::Int32, false),
+                    Field::new("price", DataType::Decimal(10, 2), false),
+                ])),
                 "person" => Ok(Schema::new(vec![
                     Field::new("id", DataType::UInt32, false),
                     Field::new("first_name", DataType::Utf8, false),
@@ -4697,6 +4701,14 @@ mod tests {
         let expected = "Projection: #person.id, #person.state, #person.age, #COUNT(UInt8(1))\
         \n  Aggregate: groupBy=[[#person.id, CUBE (#person.state, #person.age)]], aggr=[[COUNT(UInt8(1))]]\
         \n    TableScan: person projection=None";
+        quick_test(sql, expected);
+    }
+
+    #[tokio::test]
+    async fn round_decimal() {
+        let sql = "SELECT round(price/3, 2) FROM test_decimal";
+        let expected = "Projection: round(#test_decimal.price / Int64(3), Int64(2))\
+        \n  TableScan: test_decimal projection=None";
         quick_test(sql, expected);
     }
 

cube-js/arrow-datafusion

Can drop this after rebase on commit 8a29ed5 "Add SQL planner support for calling round function with two arguments (#2503)", first released in 8.0.0
(cherry picked from commit 8a29ed5b561df6bf706dc107bb49dbc8590c9b8b)
# Conflicts:
#	datafusion/core/src/sql/planner.rs
#	datafusion/expr/src/function.rs

datafusion/core/src/sql/planner.rs
@@ -5052,6 +5052,10 @@ mod tests {
             name: TableReference,
         ) -> Option<Arc<dyn TableProvider>> {
             let schema = match name.table() {
+                "test_decimal" => Some(Schema::new(vec![
+                    Field::new("id", DataType::Int32, false),
+                    Field::new("price", DataType::Decimal(10, 2), false),
+                ])),
                 "person" => Some(Schema::new(vec![
                     Field::new("id", DataType::UInt32, false),
                     Field::new("first_name", DataType::Utf8, false),
@@ -5440,6 +5444,14 @@ mod tests {
         let expected = "Projection: #person.id, #person.state, #person.age, #COUNT(UInt8(1))\
         \n  Aggregate: groupBy=[[#person.id, CUBE (#person.state, #person.age)]], aggr=[[COUNT(UInt8(1))]]\
         \n    TableScan: person projection=None";
+        quick_test(sql, expected);
+    }
+
+    #[tokio::test]
+    async fn round_decimal() {
+        let sql = "SELECT round(price/3, 2) FROM test_decimal";
+        let expected = "Projection: round(#test_decimal.price / Int64(3), Int64(2))\
+        \n  TableScan: test_decimal projection=None";
         quick_test(sql, expected);
     }
 